407b780d5b84abe0f7ffec711781782735c195f2
RenataCehajic <renata.cehajic@gmail.com>
2021-03-16 20:34:14 +0100

    Add sorting methods (parameterized selector)

--- src/components/TeachersList.js
+++ src/components/TeachersList.js
@@ -1,11 +1,25 @@
 import { useSelector } from "react-redux";
-import { selectAllTeachers } from "../store/teacher/selectors";
+import { selectAllTeachersSortedBy } from "../store/teacher/selectors";
+import { useState } from "react";
 
 export default function TeachersList() {
-  const teachers = useSelector(selectAllTeachers);
+  const [selectedSortingMethod, setSelectedSortingMethod] = useState("name");
+  // const teachers = useSelector(selectAllTeachers);
+
+  //parameterized selectors
+  const teachers = useSelector(
+    selectAllTeachersSortedBy(selectedSortingMethod)
+  );
   return (
     <>
       <h3>Teachers:</h3>
+      <select
+        value={selectedSortingMethod}
+        onChange={(e) => setSelectedSortingMethod(e.target.value)}
+      >
+        <option value="name">First name</option>
+        <option value="lastName">Last name</option>
+      </select>
       <ul>
         {teachers.map((teacher) => (
           <li key={teacher.email}>
--- src/store/teacher/selectors.js
+++ src/store/teacher/selectors.js
@@ -1,1 +1,23 @@
-export const selectAllTeachers = (reduxState) => reduxState.teacher.allTeachers;
+// export const selectAllTeachers = (reduxState) => reduxState.teacher.allTeachers;
+
+export const selectAllTeachers = (reduxState) => {
+  return reduxState.teacher.allTeachers;
+};
+
+// export const selectAllTeachersSortedBy = (sortingMethod) => (reduxState) =>
+//   reduxState.teacher.allTeachers;
+
+export const selectAllTeachersSortedBy = (sortingMethod) => {
+  return (reduxState) => {
+    // reduxState.teacher.allTeachers;
+    const teachers = [...reduxState.teacher.allTeachers];
+
+    return teachers.sort((teacherA, teacherB) => {
+      if (sortingMethod === "name") {
+        return teacherA.name.localeCompare(teacherB.name);
+      }
+
+      return teacherA.lastName.localeCompare(teacherB.lastName);
+    });
+  };
+};
